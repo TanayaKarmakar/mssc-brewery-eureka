f4bb0be97ae5effece36176fa2f2b70d8ff5df89
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-06-20 12:15:55 +0530

    added basic authentication for eureka

---
+++ src/main/java/com/app/brewery/eureka/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.app.brewery.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author t0k02w6 on 20/06/21
+ * @project mssc-brewery-eureka
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
